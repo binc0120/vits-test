d5bd6b647d4c59c67689d967667552b36328b552
binc0120 <61765744+binc0120@users.noreply.github.com>
2022-08-25 15:19:57 -0400

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -9,13 +9,27 @@ _letters = 'AEINOQUabdefghijkmnoprstuvwyz'
 _letters_ipa = 'ʃʧ↓↑ '
 
 
-# Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+# japanese_cleaners2
+_pad        = '_'
+_punctuation = ',.!?-~…'
+_letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
 
-# Special symbol ids
-SPACE_ID = symbols.index(" ")
+
 
 '''
 symbols = list(' !"&*,-.?ABCINU[]abcdefghijklmnoprstuwyz{}~')
 SPACE_ID = symbols.index(" ")
 '''
+'''
+# Export all symbols:
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
+'''
+
+# Export all symbols:
+symbols = [_pad] + list(_punctuation) + list(_letters)
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
